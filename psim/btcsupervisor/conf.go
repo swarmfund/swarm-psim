1b1275986332d64408baff3ed2e074e4a09865b9
Andrew Stepko <andrew.stepko@gmail.com>
2018-01-29 17:47:22 +0200

    Added min_deposit_amount and deposit_asset to bitcoin_supervisor config.

--- psim/btcsupervisor/conf.go
+++ psim/btcsupervisor/conf.go
@@ -10,7 +10,9 @@ type Config struct {
 
 	LastProcessedBlock uint64 `fig:"last_processed_block"`
 	LastBlocksNotWatch uint64 `fig:"last_blocks_not_watch"`
+	MinDepositAmount   uint64 `fig:"min_deposit_amount"`
+	DepositAsset       string `fig:"deposit_asset"`
 
 	// TODO Remove after moving logic of the second signature to the verifier.
-	AdditionalSignerKP   keypair.Full    `fig:"additional_signer" mapstructure:"signer"`
+	AdditionalSignerKP keypair.Full `fig:"additional_signer" mapstructure:"signer"`
 }
