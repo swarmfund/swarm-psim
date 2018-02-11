34c4e479dbb37ae5edebac32763eb48c419c2ffc
Yevhenii Luhanskyi <y.luhanskyi@gmail.com>
2018-02-11 12:01:13 +0200

    WIP: ratesync

---
+++ psim/ratesync/provider/coinmarketcap/connector.go
@@ -0,0 +1,96 @@
+package coinmarketcap
+
+import (
+	"encoding/json"
+	"fmt"
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/swarmfund/go/amount"
+	"gitlab.com/swarmfund/psim/psim/ratesync/price"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+var assetPairs = map[string]string{
+	"BTC/USD": "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD",
+	"ETH/USD": "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=USD",
+	"SWM/USD": "https://api.coinmarketcap.com/v1/ticker/swarm-fund/?convert=USD",
+}
+
+// CoinMarCapConnector is a connector for coinmarketcap.com
+type CoinMarCapConnector struct {
+	Name string
+}
+
+// NewCoinMarCapConnector is a constructor for CoinMarCapConnector
+func NewCoinMarCapConnector() *CoinMarCapConnector {
+	return &CoinMarCapConnector{
+		Name: "coinmarketcap",
+	}
+}
+
+// GetName retrieves the name of connector
+func (c *CoinMarCapConnector) GetName() string {
+	return c.Name
+}
+
+// GetPrices retrieves prices from external service and returns structured prices
+func (c *CoinMarCapConnector) GetPrices(baseAsset, quoteAsset string) (price.Prices, error) {
+	assetPair := baseAsset + "/" + quoteAsset
+	if _, ok := assetPairs[assetPair]; !ok {
+		return nil, fmt.Errorf("uknown asset pair: %v", assetPair)
+	}
+
+	request := assetPairs[assetPair]
+	response, err := http.Get(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("failed to get price with status code: %d", response.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read data from response body")
+	}
+
+	var jp jsonPrices
+	err = json.Unmarshal(body, &jp)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal response body")
+	}
+
+	result, err := jp.Prices()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal prices")
+	}
+	return result, nil
+}
+
+// jsonAssetPrice is middle-layer structure for custom JSON unmarshalling
+type jsonAssetPrice struct {
+	PriceUsd    string `json:"price_usd"`
+	LastUpdated int64  `json:"last_updated,string"`
+}
+
+// jsonPrices is an array of jsonAssetPrice
+type jsonPrices []jsonAssetPrice
+
+// Prices returns unmarshaled array of PricePoint with appropriate representation of price and time
+func (jps jsonPrices) Prices() (price.Prices, error) {
+	var result price.Prices
+	for _, jp := range jps {
+		p, err := amount.Parse(jp.PriceUsd)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse amount")
+		}
+
+		result = append(result, price.PricePoint{
+			Price: p,
+			Time:  time.Unix(jp.LastUpdated, 0),
+		})
+	}
+	return result, nil
+}
