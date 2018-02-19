a7d68b7c8443ae75ca5b5a6f7c4c42858798552d
Andrew Stepko <andrew.stepko@gmail.com>
2018-02-19 19:32:39 +0200

    Added common verify package.

---
+++ psim/verify/api.go
@@ -0,0 +1,78 @@
+package verify
+
+import (
+	"context"
+	"net/http"
+
+	"gitlab.com/distributed_lab/logan/v3"
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/swarmfund/go/xdr"
+	"gitlab.com/swarmfund/psim/ape"
+	"gitlab.com/swarmfund/psim/ape/problems"
+	"gitlab.com/swarmfund/psim/psim/verification"
+)
+
+// ServeAPI is blocking method.
+func (s *Service) serveAPI(ctx context.Context) {
+	r := ape.DefaultRouter()
+
+	r.Post("/", s.handle)
+
+	s.log.WithField("address", s.listener.Addr().String()).Info("Listening.")
+
+	err := ape.ListenAndServe(ctx, s.listener, r)
+	if err != nil {
+		s.log.WithError(err).Error("ListenAndServe returned error.")
+		return
+	}
+	return
+}
+
+func (s *Service) handle(w http.ResponseWriter, r *http.Request) {
+	req, ok := s.verifier.ReadRequest(w, r)
+	if !ok {
+		return
+	}
+
+	logger := s.log.WithFields(logan.F{
+		"request": req,
+	})
+
+	verifyErr, err := s.verifier.VerifyRequest(req)
+	if err != nil {
+		logger.WithError(err).Error("Failed to validate Request TX.")
+		ape.RenderErr(w, r, problems.ServerError(err))
+		return
+	}
+	if verifyErr != nil {
+		logger.WithField("verify_err", verifyErr).Warn("Received Request which can't pass verification - responding 403-Forbidden.")
+		ape.RenderErr(w, r, problems.Forbidden(verifyErr.Error()))
+		return
+	}
+
+	signedEnvelopeString, err := s.signAndMarshalEnvelope(req.GetEnvelope())
+	if err != nil {
+		logger.WithError(err).Error("Failed sign or marshal Envelope.")
+		ape.RenderErr(w, r, problems.ServerError(err))
+		return
+	}
+
+	ok = verification.RenderResponseEnvelope(s.log, w, r, signedEnvelopeString)
+	if ok {
+		logger.Info("Verified Request successfully.")
+	}
+}
+
+func (s *Service) signAndMarshalEnvelope(envelope xdr.TransactionEnvelope) (string, error) {
+	fullySignedEnvelope, err := s.xdrbuilder.Sign(&envelope, s.signer)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to sign Envelope")
+	}
+
+	envelopeBase64, err := xdr.MarshalBase64(fullySignedEnvelope)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to marshal fully signed Envelope")
+	}
+
+	return envelopeBase64, nil
+}
---
+++ psim/verify/service.go
@@ -0,0 +1,93 @@
+package verify
+
+import (
+	"context"
+	"net"
+	"time"
+
+	"gitlab.com/distributed_lab/discovery-go"
+	"gitlab.com/distributed_lab/logan/v3"
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/distributed_lab/logan/v3/fields"
+	"gitlab.com/swarmfund/go/xdrbuild"
+	"gitlab.com/swarmfund/psim/psim/app"
+	"gitlab.com/tokend/keypair"
+	"net/http"
+	"gitlab.com/swarmfund/go/xdr"
+)
+
+type Verifier interface {
+	ReadRequest(w http.ResponseWriter, r *http.Request) (Request, bool)
+	VerifyRequest(r Request) (checkErr, err error)
+}
+
+type Request interface {
+	GetEnvelope() xdr.TransactionEnvelope
+	fields.Provider
+}
+
+type Service struct {
+	serviceName string
+	log         *logan.Entry
+
+	verifier   Verifier
+	xdrbuilder *xdrbuild.Builder
+	signer     keypair.Full
+	listener   net.Listener
+
+	discoveryRegisterPeriod time.Duration
+	// TODO Interface
+	discovery        *discovery.Client
+	discoveryService *discovery.Service
+}
+
+func New(
+	serviceName string,
+	discoveryID string,
+	log *logan.Entry,
+	verifier Verifier,
+	builder *xdrbuild.Builder,
+	signer keypair.Full,
+	listener net.Listener,
+	discoveryClient *discovery.Client) *Service {
+
+	discoveryRegisterPeriod := 5 * time.Second
+
+	return &Service{
+		serviceName: serviceName,
+		log:         log.WithField("service", serviceName),
+
+		verifier:   verifier,
+		xdrbuilder: builder,
+		signer:     signer,
+		listener:   listener,
+
+		discoveryRegisterPeriod: discoveryRegisterPeriod,
+		discovery:               discoveryClient,
+		discoveryService: discoveryClient.Service(&discovery.ServiceRegistration{
+			Name:            serviceName,
+			ID:              discoveryID,
+			TTL:             2 * discoveryRegisterPeriod,
+			DeregisterAfter: 3 * discoveryRegisterPeriod,
+
+			Host: "http://" + listener.Addr().String(),
+		}),
+	}
+}
+
+// TODO Comment
+func (s *Service) Run(ctx context.Context) {
+	// TODO Wait for acquireLeadershipEndlessly on shutdown
+	go app.RunOverIncrementalTimer(ctx, s.log, s.serviceName+"_discovery_reregisterer", s.ensureServiceInDiscoveryOnce,
+		s.discoveryRegisterPeriod, s.discoveryRegisterPeriod/2)
+
+	s.serveAPI(ctx)
+}
+
+func (s *Service) ensureServiceInDiscoveryOnce(ctx context.Context) error {
+	_, err := s.discovery.EnsureServiceRegistered(s.discoveryService)
+	if err != nil {
+		return errors.Wrap(err, "Failed to ensure service registered in Discovery")
+	}
+	return nil
+}
