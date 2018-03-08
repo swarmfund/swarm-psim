bd297e1b8a0a42b0e514e1ad731f8e2918899497
Andrew Stepko <andrew.stepko@gmail.com>
2018-03-08 16:15:41 +0200

    Moved obtaining BalanceID into common aridrop package.

---
+++ psim/airdrop/balance.go
@@ -0,0 +1,29 @@
+package airdrop
+
+import (
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/swarmfund/horizon-connector/v2"
+)
+
+var (
+	ErrNoBalanceID  = errors.New("BalanceID not found for Account.")
+)
+
+type AccountsConnector interface {
+	Balances(address string) ([]horizon.Balance, error)
+}
+
+func GetBalanceID(accAddress, asset string, accConnector AccountsConnector) (string, error) {
+	balances, err := accConnector.Balances(accAddress)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to get Account Balances")
+	}
+
+	for _, b := range balances {
+		if b.Asset == asset {
+			return b.BalanceID, nil
+		}
+	}
+
+	return "", ErrNoBalanceID
+}
--- psim/airdrop/earlybird/manage_generals.go
+++ psim/airdrop/earlybird/manage_generals.go
@@ -7,12 +7,12 @@ import (
 
 	"gitlab.com/distributed_lab/logan/v3"
 	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/swarmfund/psim/psim/airdrop"
 	"gitlab.com/swarmfund/psim/psim/app"
 )
 
 var (
 	errUserNotFound = errors.New("User not found.")
-	errNoBalanceID  = errors.New("BalanceID not found for Account.")
 )
 
 func (s *Service) consumeGeneralAccounts(ctx context.Context) {
@@ -74,23 +74,6 @@ func (s *Service) processGeneralAccount(ctx context.Context, accAddress string) 
 	return nil
 }
 
-func (s *Service) processIssuance(ctx context.Context, accAddress, email string) error {
-	balanceID, err := s.getBalanceID(accAddress)
-	if err != nil {
-		return errors.Wrap(err, "Failed to get BalanceID of the Account")
-	}
-	fields := logan.F{"balance_id": balanceID}
-
-	_, err = s.submitIssuance(ctx, accAddress, balanceID)
-	if err != nil {
-		return errors.Wrap(err, "Failed to process Issuance", fields)
-	}
-
-	s.emails.Put(email)
-
-	return nil
-}
-
 // IsReadyForIssuance returns empty email without error, if Account is not ready yet.
 func (s *Service) isReadyForIssuance(accountAddress string) (email string, err error) {
 	user, err := s.usersConnector.User(accountAddress)
@@ -110,17 +93,19 @@ func (s *Service) isReadyForIssuance(accountAddress string) (email string, err e
 	}
 }
 
-func (s *Service) getBalanceID(accAddress string) (string, error) {
-	balances, err := s.accountsConnector.Balances(accAddress)
+func (s *Service) processIssuance(ctx context.Context, accAddress, email string) error {
+	balanceID, err := airdrop.GetBalanceID(accAddress, s.config.Asset, s.accountsConnector)
 	if err != nil {
-		return "", errors.Wrap(err, "Failed to get Account Balances")
+		return errors.Wrap(err, "Failed to get BalanceID of the Account")
+	}
+	fields := logan.F{"balance_id": balanceID}
+
+	_, err = s.submitIssuance(ctx, accAddress, balanceID)
+	if err != nil {
+		return errors.Wrap(err, "Failed to process Issuance", fields)
 	}
 
-	for _, b := range balances {
-		if b.Asset == s.config.Asset {
-			return b.BalanceID, nil
-		}
-	}
+	s.emails.Put(email)
 
-	return "", errNoBalanceID
+	return nil
 }
