a5ed6d9ef60787f9d6897545b62ae063027961a0
Yevhenii Luhanskyi <y.luhanskyi@gmail.com>
2018-03-21 12:34:17 +0200

    Unit tests for order notifier refactored

--- psim/ordernotifier/order_notifier_test.go
+++ psim/ordernotifier/order_notifier_test.go
@@ -30,112 +30,108 @@ func TestOrderNotifierHelper(t *testing.T) {
 
 	s := New(config, transactionConnector, emailSender, logger, userConnector, saleConnector, make(chan horizon.CheckSaleStateResponse, 0))
 
-	Convey("Ineligible effect", t, func() {
-		checkSaleStateOp := horizon.CheckSaleState{
-			SaleID:        1,
-			Effect:        xdr.CheckSaleStateEffectClosed.String(),
-			TransactionID: "some_transaction_id",
-		}
-		err := s.processCheckSaleStateOperation(ctx, checkSaleStateOp)
-		So(err, ShouldBeNil)
-	})
-	Convey("Transaction doesn't exist", t, func() {
-		defer transactionConnector.AssertExpectations(t)
+	var saleID uint64 = 1
+	var accountID xdr.AccountId
+	err := accountID.SetAddress("GDDDAXSERVQRFM3STE65GNLWJ7A6QHOHDGJLSGFZBKETSPGR3RHI5TXT")
+	if err != nil {
+		t.Fatal(err)
+	}
+	offer := xdr.LedgerKeyOffer{
+		OwnerId: accountID,
+		OfferId: 1,
+	}
 
-		checkSaleStateOp := horizon.CheckSaleState{
-			SaleID:        1,
-			Effect:        xdr.CheckSaleStateEffectUpdated.String(),
-			TransactionID: "some_transaction_id",
-		}
-
-		transactionConnector.On("TransactionByID", checkSaleStateOp.TransactionID).Return(nil, nil).Once()
-
-		err := s.processCheckSaleStateOperation(ctx, checkSaleStateOp)
-		So(err, ShouldBeNil)
-	})
-	Convey("Wrong change type", t, func() {
+	Convey("Process ledger entry", t, func() {
 		change := xdr.LedgerEntryChange{
-			Type: xdr.LedgerEntryChangeTypeCreated,
+			Type: xdr.LedgerEntryChangeTypeUpdated,
 		}
 
-		_, err := s.processLedgerEntry(ctx, change, 1)
+		Convey("Wrong LedgerEntryChange", func() {
+			s.processLedgerEntry(ctx, change, saleID)
+			userConnector.AssertExpectations(t)
+		})
+	})
+
+	Convey("Unique token", t, func() {
+		emailAddress := "test_mail@gmail.com"
+		var offerID uint64 = 1
+		var saleID uint64 = 2
+		expectedUniqueToken := fmt.Sprintf("%s:%d:%d:%s", emailAddress, offerID, saleID, s.config.RequestTokenSuffix)
+		Convey("Expected unique token returns", func() {
+			actualUniqueToken := s.buildUniqueToken(emailAddress, offerID, saleID)
+			So(expectedUniqueToken, ShouldEqual, actualUniqueToken)
+		})
+		Convey("Request token suffix has changed", func() {
+			s.config.RequestTokenSuffix += "s"
+			actualUniqueToken := s.buildUniqueToken(emailAddress, offerID, saleID)
+			So(expectedUniqueToken, ShouldNotEqual, actualUniqueToken)
+		})
+	})
+
+	Convey("Email message build", t, func() {
+		saleName := "Test sale"
+		Convey("Template not found", func() {
+			s.config.TemplateName += "s"
+			_, err := s.buildEmailMessage(saleName)
+			So(err, ShouldNotBeNil)
+		})
+		Convey("Successful build", func() {
+			s.config.TemplateName = "io_cancelled.html"
+			msg, err := s.buildEmailMessage(saleName)
+			So(err, ShouldBeNil)
+			So(msg, ShouldContainSubstring, config.TemplateLinkURL)
+			So(msg, ShouldContainSubstring, saleName)
+		})
+	})
+
+	Convey("User doesn't exist", t, func() {
+		defer userConnector.AssertExpectations(t)
+		defer saleConnector.AssertExpectations(t)
+
+		userConnector.On("User", offer.OwnerId.Address()).Return(nil, nil).Once()
+		emailUnit, err := s.processCancelledOrder(ctx, offer, saleID)
+
+		So(emailUnit, ShouldBeNil)
 		So(err, ShouldBeNil)
 	})
-	Convey("Wrong entry ", t, func() {
-		change := xdr.LedgerEntryChange{
-			Type: xdr.LedgerEntryChangeTypeRemoved,
-			Removed: &xdr.LedgerKey{
-				Type: xdr.LedgerEntryTypeAccount,
+	Convey("User exists", t, func() {
+		user := &horizon.User{
+			Type: "General",
+			ID:   "User id",
+			Attributes: horizon.UserAttributes{
+				Email: "test_mail@gmail.com",
 			},
 		}
 
-		_, err := s.processLedgerEntry(ctx, change, 1)
-		So(err, ShouldBeNil)
-	})
-	Convey("User doesn't exist", t, func() {
-		var saleID uint64 = 1
-		var accountID xdr.AccountId
-		err := accountID.SetAddress("GDDDAXSERVQRFM3STE65GNLWJ7A6QHOHDGJLSGFZBKETSPGR3RHI5TXT")
-		offer := xdr.LedgerKeyOffer{
-			OwnerId: accountID,
-			OfferId: 1,
-		}
+		userConnector.On("User", offer.OwnerId.Address()).Return(user, nil).Once()
 
-		userConnector.On("User", offer.OwnerId.Address()).Return(nil, nil).Once()
+		Convey("Sale doesn't exist", func() {
+			saleConnector.On("SaleByID", saleID).Return(nil, nil).Once()
 
-		emailUnit, err := s.processCancelledOrder(ctx, offer, saleID)
-		userConnector.AssertExpectations(t)
-		So(emailUnit, ShouldBeNil)
-		So(err, ShouldBeNil)
-
-		Convey("User exists", func() {
-			user := &horizon.User{
-				Type: "General",
-				ID:   "User id",
-				Attributes: horizon.UserAttributes{
-					Email: "test_mail@gmail.com",
+			emailUnit, err := s.processCancelledOrder(ctx, offer, saleID)
+			So(emailUnit, ShouldBeNil)
+			So(err, ShouldBeNil)
+		})
+		Convey("Sale exist", func() {
+			sale := &horizon.Sale{
+				ID: saleID,
+				Details: horizon.SaleDetails{
+					Name: "Test sale",
 				},
 			}
 
+			msg, err := s.buildEmailMessage(sale.Name())
+			So(err, ShouldBeNil)
+
 			userConnector.On("User", offer.OwnerId.Address()).Return(user, nil).Once()
-			defer userConnector.AssertExpectations(t)
+			saleConnector.On("SaleByID", saleID).Return(sale, nil).Once()
 
-			Convey("Valid unique token", func() {
-				expectedUniqueToken := fmt.Sprintf("%s:%d:%d:%s", user.Attributes.Email, offer.OfferId, saleID, s.config.RequestTokenSuffix)
-				actualUniqueToken := s.buildUniqueToken(user.Attributes.Email, uint64(offer.OfferId), uint64(saleID))
-				So(expectedUniqueToken, ShouldEqual, actualUniqueToken)
-
-				Convey("Sale doesn't exist", func() {
-					saleConnector.On("SaleByID", saleID).Return(nil, nil).Once()
-					defer saleConnector.AssertExpectations(t)
-
-					emailUnit, err = s.processCancelledOrder(ctx, offer, saleID)
-					So(emailUnit, ShouldBeNil)
-					So(err, ShouldBeNil)
-
-					Convey("Sale exists", func() {
-						sale := &horizon.Sale{
-							ID: saleID,
-							Details: horizon.SaleDetails{
-								Name: "Test sale",
-							},
-						}
-
-						msg, err := s.buildEmailMessage(sale.Name())
-						So(err, ShouldBeNil)
-
-						userConnector.On("User", offer.OwnerId.Address()).Return(user, nil).Once()
-						saleConnector.On("SaleByID", saleID).Return(sale, nil).Once()
-
-						emailUnit, err = s.processCancelledOrder(ctx, offer, saleID)
-						So(err, ShouldBeNil)
-						So(emailUnit.UniqueToken, ShouldEqual, expectedUniqueToken)
-						So(emailUnit.Payload.Destination, ShouldEqual, user.Attributes.Email)
-						So(emailUnit.Payload.Subject, ShouldEqual, s.config.Subject)
-						So(emailUnit.Payload.Message, ShouldEqual, msg)
-					})
-				})
-			})
+			emailUnit, err := s.processCancelledOrder(ctx, offer, saleID)
+			So(err, ShouldBeNil)
+			So(emailUnit.UniqueToken, ShouldEqual, s.buildUniqueToken(user.Attributes.Email, uint64(offer.OfferId), saleID))
+			So(emailUnit.Payload.Destination, ShouldEqual, user.Attributes.Email)
+			So(emailUnit.Payload.Subject, ShouldEqual, s.config.Subject)
+			So(emailUnit.Payload.Message, ShouldEqual, msg)
 		})
 	})
 }
