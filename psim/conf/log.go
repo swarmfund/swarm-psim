f5322d6f1ba0682cd8056324d4ea8ad9c82f9df1
Andrew Stepko <andrew.stepko@gmail.com>
2018-04-17 17:33:59 +0300

    Add 'raw_error' field during SentryHook to duplicate the error so that it be readable if long.

--- psim/conf/log.go
+++ psim/conf/log.go
@@ -25,7 +25,7 @@ func (c *ViperConfig) Log() (*logan.Entry, error) {
 
 	v := c.viper.Sub("log")
 	if v == nil {
-		return nil, errors.New("log config is required")
+		return nil, errors.New("Log config is required.")
 	}
 
 	entry := logan.New()
@@ -35,7 +35,7 @@ func (c *ViperConfig) Log() (*logan.Entry, error) {
 	}
 	lvl, err := logan.ParseLevel(level)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse level")
+		return nil, errors.Wrap(err, "Failed to parse log level")
 	}
 	entry = entry.Level(lvl)
 
@@ -45,12 +45,12 @@ func (c *ViperConfig) Log() (*logan.Entry, error) {
 
 	err = addSlackHook(v, entry)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "Failed to add Slack hook")
 	}
 
 	err = addSentryHook(v, entry)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "Failed to add Sentry hook")
 	}
 
 	// set log formatter
@@ -112,13 +112,31 @@ func addSentryHook(v *viper.Viper, entry *logan.Entry) error {
 		logrus.ErrorLevel,
 	})
 	if err != nil {
-		return errors.Wrap(err, "Failed to initialize sentry")
+		return errors.Wrap(err, "Failed to create Sentry hook")
 	}
 
 	hook.StacktraceConfiguration.Enable = true
+	// TODO Consider using log level from config
 	hook.StacktraceConfiguration.Level = logrus.ErrorLevel
 	hook.StacktraceConfiguration.Context = NLinesAroundErrorPoint
 
-	entry.AddLogrusHook(hook)
+	wrapperHook := sentryWrapperHook{
+		SentryHook: hook,
+	}
+
+	entry.AddLogrusHook(&wrapperHook)
 	return nil
+}
+
+type sentryWrapperHook struct {
+	*logrus_sentry.SentryHook
+}
+
+func (h *sentryWrapperHook) Fire(entry *logrus.Entry) error {
+	err, ok := entry.Data[logan.ErrorKey]
+	if ok {
+		entry.Data["raw_error"] = err
+	}
+
+	return h.SentryHook.Fire(entry)
 }
