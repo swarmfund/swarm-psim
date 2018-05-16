910cddfce744580df0470a96c019efc861d96306
Andrew Stepko <andrew.stepko@gmail.com>
2018-05-16 16:48:35 +0300

    Update horizon-connector to 3.4.

--- vendor/gitlab.com/tokend/horizon-connector/internal/listener/reviewable_requests.go
+++ vendor/gitlab.com/tokend/horizon-connector/internal/listener/reviewable_requests.go
@@ -38,14 +38,14 @@ func (q *Q) StreamWithdrawalRequests(ctx context.Context) (<-chan ReviewableRequ
 
 // StreamWithdrawalRequestsOfAsset streams all Withdraw and TwoStepWithdraw ReviewableRequests
 // with filter by provided destAssetCode
-func (q *Q) StreamWithdrawalRequestsOfAsset(ctx context.Context, destAssetCode string, reverseOrder, endlessly bool) (<-chan ReviewableRequestEvent) {
+func (q *Q) StreamWithdrawalRequestsOfAsset(ctx context.Context, destAssetCode string, reverseOrder bool) (<-chan ReviewableRequestEvent) {
 	getParams := fmt.Sprintf("dest_asset_code=%s", destAssetCode)
 
 	if reverseOrder {
 		getParams += "&order=desc"
 	}
 
-	return q.getAndStreamReviewableRequests(ctx, getParams, "", operation.WithdrawalsReviewableRequestType, !endlessly)
+	return q.getAndStreamReviewableRequests(ctx, getParams, "", operation.WithdrawalsReviewableRequestType, false)
 }
 
 func (q *Q) getAndStreamReviewableRequests(ctx context.Context, getParams, cursor string, reqType operation.ReviewableRequestType, stopOnEmptyPage bool) (<-chan ReviewableRequestEvent) {
