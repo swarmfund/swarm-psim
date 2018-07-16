c878e4d7b5fa67e73f5af91b29ce43e7738af0b1
Oleksii Khramov <khramoff.al@gmail.com>
2018-07-16 16:17:08 +0300

    Minor refactoring

--- psim/withdrawals/dashwithdraw/greedy_coin_selector.go
+++ psim/withdrawals/dashwithdraw/greedy_coin_selector.go
@@ -3,16 +3,15 @@ package dashwithdraw
 import (
 	"sync"
 
+	"math/rand"
+
 	"gitlab.com/distributed_lab/logan/v3/errors"
 	"gitlab.com/swarmfund/psim/psim/bitcoin"
-	"math/rand"
 )
 
 var (
 	ErrInsufficientFunds = errors.New("Insufficient funds.")
 )
-
-
 
 type GreedyCoinSelector struct {
 	dustThreshold int64
@@ -44,9 +43,6 @@ func (s GreedyCoinSelector) Fund(amount int64) (utxos []bitcoin.Out, change int6
 		return nil, 0, ErrInsufficientFunds
 	}
 
-	var totalFunded int64
-	var result map[bitcoin.Out]UTXO
-
 	activeUTXOS := s.getActiveUTXOs()
 
 	if len(activeUTXOS) == 0 {
@@ -54,6 +50,8 @@ func (s GreedyCoinSelector) Fund(amount int64) (utxos []bitcoin.Out, change int6
 		return nil, 0, ErrInsufficientFunds
 	}
 
+	var totalFunded int64
+	var result map[bitcoin.Out]UTXO
 	result, totalFunded = s.chooseUTXOs(activeUTXOS, amount)
 
 	return getKeys(result), totalFunded - amount, nil
@@ -70,28 +68,31 @@ func (s GreedyCoinSelector) chooseUTXOs(utxos map[bitcoin.Out]UTXO, amountToFill
 	}
 
 	//Ideal UTXO not found, splitting into 2 groups
-	lesser := make(map[bitcoin.Out]UTXO)
-	greater := make(map[bitcoin.Out]UTXO)
-	var lesserAmount int64
+	smallerUTXOS := make(map[bitcoin.Out]UTXO)
+	biggerUTXOS := make(map[bitcoin.Out]UTXO)
+	var totalSmalls int64
 
-	for k, v := range utxos{
-		if v.Value < amountToFill{
-			lesser[k] = v
-			lesserAmount += v.Value
+	//Split UTXOs into 2 groups - the ones that bigger than amountToFill,
+	//and the ones that smaller than amountToFill
+	//also we check whether sum of all small utxos is greater or equal to amountToFill
+	for k, v := range utxos {
+		if v.Value < amountToFill {
+			smallerUTXOS[k] = v
+			totalSmalls += v.Value
 			continue
 		}
-		greater[k] = v
+		biggerUTXOS[k] = v
 	}
 
-	//If small UTXOs can fullfil amount
-	if lesserAmount >= amountToFill{
-		keys := getKeys(lesser)
-		permutations, isEmpty := s.getPermutations(lesser, keys, amountToFill)
+	//If small UTXOs can fulfil amount
+	if totalSmalls >= amountToFill {
+		keys := getKeys(smallerUTXOS)
+		permutations, isEmpty := s.getPermutations(smallerUTXOS, keys, amountToFill)
 
-		if !isEmpty{
+		if !isEmpty {
 			indexToChoose := rand.Intn(len(permutations))
 			sum := sumOfUTXOs(permutations[indexToChoose])
-			for i, p := range permutations{
+			for i, p := range permutations {
 				newSum := sumOfUTXOs(p)
 				if newSum < sum {
 					sum = newSum
@@ -103,29 +104,28 @@ func (s GreedyCoinSelector) chooseUTXOs(utxos map[bitcoin.Out]UTXO, amountToFill
 		}
 	}
 
-	//choosing one minimal UTXO from the ones that greater than amount
-	minOut := chooseRandomInMap(greater)
-	for i, utxo := range greater{
-		if utxo.Value < greater[minOut].Value {
+	//choosing one minimal UTXO from the ones that biggerUTXOS than amount
+	minOut := chooseRandomInMap(biggerUTXOS)
+	for i, utxo := range biggerUTXOS {
+		if utxo.Value < biggerUTXOS[minOut].Value {
 			minOut = i
 		}
 	}
 	result := make(map[bitcoin.Out]UTXO)
-	result[minOut] = greater[minOut]
+	result[minOut] = biggerUTXOS[minOut]
 
 	return result, result[minOut].Value
 }
 
-func sumOfUTXOs(utxos map[bitcoin.Out]UTXO) int64{
+func sumOfUTXOs(utxos map[bitcoin.Out]UTXO) int64 {
 	var sum int64
-	for _, v := range utxos{
+	for _, v := range utxos {
 		sum += v.Value
 	}
 	return sum
 }
 
-
-func chooseRandomInMap(utxos map[bitcoin.Out]UTXO) bitcoin.Out{
+func chooseRandomInMap(utxos map[bitcoin.Out]UTXO) bitcoin.Out {
 	indexToChoose := rand.Intn(len(utxos))
 	var i int
 	for k := range utxos {
@@ -137,35 +137,35 @@ func chooseRandomInMap(utxos map[bitcoin.Out]UTXO) bitcoin.Out{
 	return bitcoin.Out{}
 }
 
-func (s GreedyCoinSelector) getPermutations(utxos map[bitcoin.Out]UTXO, keys []bitcoin.Out, amountToFill int64) ([]map[bitcoin.Out]UTXO, bool){
-	rounds := min(1 << uint(len(utxos)), 1000)
+func (s GreedyCoinSelector) getPermutations(utxos map[bitcoin.Out]UTXO, keys []bitcoin.Out, amountToFill int64) ([]map[bitcoin.Out]UTXO, bool) {
+	rounds := min(1<<uint(len(utxos)), 1000)
 
 	options := powerSet(rounds, keys)
 
 	result := make([]map[bitcoin.Out]UTXO, 0, rounds)
 
-	for _, option := range options{
+	for _, option := range options {
 
 		combination := make(map[bitcoin.Out]UTXO)
 		var sum int64
-		for _, key := range option{
+		for _, key := range option {
 			combination[key] = utxos[key]
 			sum += utxos[key].Value
 		}
-		if sum < amountToFill{
+		if sum < amountToFill {
 			continue
 		}
 		result = append(result, combination)
 	}
 
 	var isEmpty bool
-	if len(result) == 0{
+	if len(result) == 0 {
 		isEmpty = true
 	}
 	return result, isEmpty
 }
 
-func getKeys(utxos map[bitcoin.Out]UTXO) []bitcoin.Out{
+func getKeys(utxos map[bitcoin.Out]UTXO) []bitcoin.Out {
 	keys := make([]bitcoin.Out, 0, len(utxos))
 	for k := range utxos {
 		keys = append(keys, k)
@@ -173,7 +173,7 @@ func getKeys(utxos map[bitcoin.Out]UTXO) []bitcoin.Out{
 
 	return keys
 }
-func powerSet(powerSetSize int, original []bitcoin.Out) [][]bitcoin.Out{
+func powerSet(powerSetSize int, original []bitcoin.Out) [][]bitcoin.Out {
 	result := make([][]bitcoin.Out, 0, powerSetSize)
 
 	var index int
@@ -181,7 +181,7 @@ func powerSet(powerSetSize int, original []bitcoin.Out) [][]bitcoin.Out{
 		var subSet []bitcoin.Out
 
 		for j, elem := range original {
-			if index& (1 << uint(j)) > 0 {
+			if index&(1<<uint(j)) > 0 {
 				subSet = append(subSet, elem)
 			}
 		}
@@ -198,7 +198,7 @@ func min(a, b int) int {
 	return b
 }
 
-func (s GreedyCoinSelector) getActiveUTXOs() map[bitcoin.Out]UTXO{
+func (s GreedyCoinSelector) getActiveUTXOs() map[bitcoin.Out]UTXO {
 	activeUTXOSCopy := make(map[bitcoin.Out]UTXO)
 	for k, v := range s.utxos {
 		if !v.IsInactive {
@@ -207,7 +207,6 @@ func (s GreedyCoinSelector) getActiveUTXOs() map[bitcoin.Out]UTXO{
 	}
 	return activeUTXOSCopy
 }
-
 
 func (s GreedyCoinSelector) TryRemoveUTXO(out bitcoin.Out) bool {
 	utxo, ok := s.utxos[out]
