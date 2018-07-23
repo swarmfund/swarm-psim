866ccbab6dde1911c52315f6e261bb712f5f5848
Oleksii Khramov <khramoff.al@gmail.com>
2018-07-23 08:13:13 +0300

    TestGetTemplate refactored

--- psim/template_provider/internal/handlers/get_template_test.go
+++ psim/template_provider/internal/handlers/get_template_test.go
@@ -11,10 +11,10 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/go-chi/chi"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"gitlab.com/distributed_lab/logan/v3"
-	"gitlab.com/distributed_lab/logan/v3/errors"
 	"gitlab.com/swarmfund/psim/psim/template_provider/internal/handlers/mocks"
 	"gitlab.com/swarmfund/psim/psim/template_provider/internal/middlewares"
 )
@@ -24,44 +24,35 @@ func TestGetTemplate(t *testing.T) {
 		name       string
 		bucket     string
 		key        string
-		actualKey  string
-		actual     string
-		expected   string
+		body       string
 		statusCode int
-		err        bool
+		err        error
 	}{
 		{
 			name:       "valid",
 			key:        "template",
-			actualKey:  "template",
 			bucket:     "bucket",
-			actual:     "This is the contents of template",
-			expected:   "This is the contents of template",
+			body:       "This is the contents of template",
 			statusCode: 200,
 		},
 		{
 			name:       "Failed to download",
 			key:        "template",
-			actualKey:  "template",
 			bucket:     "bucket",
-			actual:     "This issdkfj the",
-			expected:   "This is the",
-			err:        true,
+			err:        errors.New("Failed to download"),
 			statusCode: 500,
 		},
 		{
 			name:       "invalid key",
 			key:        "notemplate",
-			actualKey:  "template",
 			bucket:     "bucket",
-			err:        true,
+			err:        awserr.New(s3.ErrCodeNoSuchKey, "", nil),
 			statusCode: 404,
 		},
 		{
 			name:       "invalid bucket",
 			key:        "template",
-			bucket:     "bukit",
-			err:        true,
+			err:        awserr.New(s3.ErrCodeNoSuchBucket, "", nil),
 			statusCode: 500,
 		},
 	}
@@ -86,22 +77,14 @@ func TestGetTemplate(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 
 			mockDownloadFunc := func(w io.WriterAt, input *s3.GetObjectInput, options ...func(*s3manager.Downloader)) error {
-
-				if tc.actual != tc.expected {
-					return errors.New("Failed to download")
+				if tc.err != nil {
+					return tc.err
 				}
 
-				if tc.bucket != "bucket" {
-					return awserr.New(s3.ErrCodeNoSuchBucket, "No such bucket", nil)
-				}
-
-				if tc.key != tc.actualKey {
-					return awserr.New(s3.ErrCodeNoSuchKey, "", nil)
-				}
-
-				w.WriteAt([]byte(tc.actual), 0)
+				w.WriteAt([]byte(tc.body), 0)
 				return nil
 			}
+
 			downloader.On("Download",
 				mock.Anything, mock.Anything).
 				Return(int64(0), mockDownloadFunc).Once()
