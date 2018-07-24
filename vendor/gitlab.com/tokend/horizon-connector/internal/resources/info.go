679a39790717540f4df3e596165b4cb0c1de3502
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-07-24 20:39:07 +0300

    Bump vendor

--- vendor/gitlab.com/tokend/horizon-connector/internal/resources/info.go
+++ vendor/gitlab.com/tokend/horizon-connector/internal/resources/info.go
@@ -6,3 +6,15 @@ type Info struct {
 	MasterAccountID    string `json:"master_account_id"`
 	TXExpirationPeriod int64  `json:"tx_expiration_period"`
 }
+
+func (c *Info) GetMasterAccountID() string {
+	return c.MasterAccountID
+}
+
+func (c *Info) GetPassphrase() string {
+	return c.Passphrase
+}
+
+func (c *Info) GetTXExpire() int64 {
+	return c.TXExpirationPeriod
+}
