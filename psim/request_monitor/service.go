567c89120eaaa6c136d4f52e71d1f423123e0f6c
Artem <artemkaskriabin@gmail.com>
2018-08-28 14:59:04 +0000

    Send to slack notifications about stale requests, update vendor go

--- psim/request_monitor/service.go
+++ psim/request_monitor/service.go
@@ -14,16 +14,21 @@ import (
 )
 
 type Service struct {
-	config    Config
-	logger    *logan.Entry
-	connector *horizon.Connector
+	config              Config
+	logger              *logan.Entry
+	connector           *horizon.Connector
+	notifier            Notifier
+	sentStaleRequestIDs map[uint64]time.Time
 }
 
-func New(config Config, log *logan.Entry, horizonConnector *horizon.Connector) *Service {
+func New(config Config, notifier Notifier, log *logan.Entry, horizonConnector *horizon.Connector,
+) *Service {
 	return &Service{
-		config:    config,
-		logger:    log.WithField("service", conf.ServiceRequestMonitor),
-		connector: horizonConnector,
+		config:              config,
+		logger:              log.WithField("service", conf.ServiceRequestMonitor),
+		connector:           horizonConnector,
+		notifier:            notifier,
+		sentStaleRequestIDs: make(map[uint64]time.Time),
 	}
 }
 
@@ -51,15 +56,26 @@ func (s *Service) checkRequests(ctx context.Context) error {
 		}
 
 		if request.State != int32(ReviewableRequestStatePending) {
+			delete(s.sentStaleRequestIDs, request.ID)
 			continue
 		}
 
-		if s.isUnresolvedBeforeTimeout(time.Time(request.UpdatedAt), request.Details.RequestType) {
-			s.logger.WithFields(logan.F{
-				"request type":  request.Details.RequestType,
-				"request state": request.State,
-				"last update":   request.UpdatedAt,
-			}).Error("stale request")
+		if s.isItTimeToNotify(request.UpdatedAt.Time(), request.Details.RequestType, request.ID) {
+			err = s.notifier.Notify(logan.F{
+				"request type":  request.Details.RequestTypeName,
+				"request state": request.StateName,
+				"last update":   request.UpdatedAt.Time().UTC(),
+				"request_id":    request.ID,
+				"reviewer":      request.Reviewer,
+				"requestor":     request.Requestor,
+			}, "stale request")
+
+			if err != nil {
+				s.logger.WithError(err).Error("failed to notify")
+				continue
+			}
+
+			s.sentStaleRequestIDs[request.ID] = time.Now().UTC()
 		}
 	}
 
@@ -70,6 +86,17 @@ func (s *Service) isUnresolvedBeforeTimeout(updatedAt time.Time, requestType int
 	timeout := s.getTimeout(requestType)
 	elapsedTime := time.Now().Sub(updatedAt)
 	return elapsedTime > timeout
+}
+
+func (s *Service) isItTimeToNotify(updatedAt time.Time, requestType int32, requestID uint64) bool {
+	if lastNotified, ok := s.sentStaleRequestIDs[requestID]; ok {
+		notNotified := time.Now().Sub(lastNotified)
+		if notNotified < s.config.NotifyPeriod {
+			return false
+		}
+	}
+
+	return s.isUnresolvedBeforeTimeout(updatedAt, requestType)
 }
 
 func (s *Service) getTimeout(requestType int32) time.Duration {
